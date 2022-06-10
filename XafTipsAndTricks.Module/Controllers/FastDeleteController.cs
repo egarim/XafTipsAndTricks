af0665313d1583a476ca1642d6bc0627ffca081b
Jose Manuel Ojeda Melgar <joche.ojeda@bitframeworks.com>
2022-06-09 17:10:28 -0700

    Fast delete

---
+++ XafTipsAndTricks.Module/Controllers/FastDeleteController.cs
@@ -0,0 +1,83 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XafTipsAndTricks.Module.BusinessObjects;
+
+namespace XafTipsAndTricks.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class FastDeleteController : ViewController
+    {
+        SimpleAction GenerateData;
+        SimpleAction FastDeleteAction;
+        // Use CodeRush to create Controllers and Actions with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/403133/
+        public FastDeleteController()
+        {
+            InitializeComponent();
+            FastDeleteAction = new SimpleAction(this, "Fast Delete", "View");
+            FastDeleteAction.Execute += FastDeleteAction_Execute;
+
+
+            GenerateData = new SimpleAction(this, "Generate Records", "View");
+            GenerateData.Execute += GenerateData_Execute;
+            
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+        }
+        private void GenerateData_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                this.View.ObjectSpace.CreateObject<Customer>().Name = Guid.NewGuid().ToString();
+            }
+            this.View.ObjectSpace.CommitChanges();
+            // Execute your business logic (https://docs.devexpress.com/eXpressAppFramework/112737/).
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            
+            // Perform various tasks depending on the target View.
+        }
+        private void FastDeleteAction_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            var StartTime = DateTime.Now;
+          this.ObjectSpace.Delete<Customer>(new BinaryOperator("Active", true));
+            var TotalTime=(DateTime.Now -StartTime).TotalMilliseconds;
+        
+
+            MessageOptions options = new MessageOptions();
+            options.Duration = 4000;
+            options.Message = $"Records deleted in {TotalTime} milliseconds";
+            options.Type = InformationType.Success;
+            options.Web.Position = InformationPosition.Right;
+            options.Win.Caption = "Success";
+            options.Win.Type = WinMessageType.Toast;
+
+            Application.ShowViewStrategy.ShowMessage(options);
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+    }
+}
---
+++ XafTipsAndTricks.Module/Extensions/DeleteExtensions.cs
@@ -0,0 +1,49 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Xpo;
+using DevExpress.Xpo;
+using DevExpress.Xpo.DB;
+using DevExpress.Xpo.Generators;
+using DevExpress.Xpo.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class DeleteExtensions
+    {
+        public static ModificationResult Delete<T>(this IObjectSpace Os, CriteriaOperator criteria = null) where T : IXPObject
+        {
+            var XpoOs = Os as XPObjectSpace;
+            if (XpoOs == null)
+            {
+                throw new InvalidOperationException("this extension requires an XpoObjectSpace");
+            }
+            return XpoOs.Session.Delete<T>(criteria);
+
+        }
+        //HACK ZeroSharp blog http://blog.zerosharp.com/fast-batch-deletions-with-devexpress-xpo/
+        public static ModificationResult Delete<T>(this Session session, CriteriaOperator criteria = null) where T : IXPObject
+        {
+            if (ReferenceEquals(criteria, null))
+                criteria = CriteriaOperator.Parse("True");
+
+            XPClassInfo classInfo = session.GetClassInfo(typeof(T));
+            var batchWideData = new BatchWideDataHolder4Modification(session);
+            int recordsAffected = (int)session.Evaluate<T>(CriteriaOperator.Parse("Count()"), criteria);
+            //HACK now this overload gets a criteria set
+            List<ModificationStatement> collection = DeleteQueryGenerator.GenerateDelete(classInfo, ObjectGeneratorCriteriaSet.GetCommonCriteriaSet(criteria), batchWideData);
+            foreach (ModificationStatement item in collection)
+            {
+                item.RecordsAffected = recordsAffected;
+            }
+
+            ModificationStatement[] collectionToArray = collection.ToArray<ModificationStatement>();
+            ModificationResult result = session.DataLayer.ModifyData(collectionToArray);
+            return result;
+        }
+    }
+}
